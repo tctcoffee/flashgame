62ff8b19b59fcb96a55a34d0e2ab39cc0da277bc
tctcoffee@163.com <root@localhost.localdomain>
2016-05-17 16:27:27 +0800

    v1.1 rules

--- flashgame/spiders/flashgame_spider.py
+++ flashgame/spiders/flashgame_spider.py
@@ -6,23 +6,37 @@
 # mail: YourEmail
 # Created Time: 2016-05-16
 
-from scrapy.spider import Spider
+try:
+    from scrapy.spider import Spider
+except:
+    from scrapy.spider import BaseSpider as Spider
+from scrapy.utils.response import get_base_url
 from scrapy.selector import Selector
 from flashgame.items import FlashgameItem
-#from scrapy.contrib.spiders import CrawlSpider,Rule
-#from scrapy.contrib.linkextractors import LinkExtractor
+from scrapy.contrib.spiders import CrawlSpider,Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 import codecs
 
-class FlashgameSpider(Spider):
+class FlashgameSpider(CrawlSpider):
     name = "flashgame"
     allow_domains = ["psy525.cn"]
-    start_urls = []
-    for i in range(1,81):
-        newurl="http://www.psy525.cn/game/"+str(i)+".html"
-        start_urls.append(newurl)
+    start_urls = [
+            "http://www.psy525.cn/cath/game0_0.html",
+            "http://www.psy525.cn/cath/game0_16.html",
+            "http://www.psy525.cn/cath/game0_32.html",
+            "http://www.psy525.cn/cath/game0_48.html",
+            "http://www.psy525.cn/cath/game0_64.html"
+            ]
+    rules = [Rule(SgmlLinkExtractor(allow=(r'http://www.psy525.cn/game/\d{1,2}\.html')),callback="parse_url",follow=True)]
 
-    def parse(self,response):
+    #for i in range(1,81):
+    #    newurl="http://www.psy525.cn/game/"+str(i)+".html"
+    #    start_urls.append(newurl)
+
+    def parse_url(self,response):
         sel = Selector(response)
+        base_url = get_base_url(response)
+        print base_url
         items = FlashgameItem()
         items["name"] = sel.xpath('//div[@id="gameTxt"]/h1/text()').extract()[0].encode('utf-8')
         items["description"] = sel.xpath('//div[@id="gameTxt"]/span/em/text()').extract()[0].encode('utf-8')
